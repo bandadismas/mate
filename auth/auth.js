4dedb9dfb5149fb9efa23f6cdf404d3e5d860f55
Dismas <bandadismas@gmail.com>
2021-03-05 12:26:12 +0200

    Create a post

--- auth/auth.js
+++ auth/auth.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
-const secret = 'test';
+const secret = 'SECRET';
 
 const auth = async (req, res, next) => {
   try {
