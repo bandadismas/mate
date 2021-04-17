d765a30ac3e10bab33d91526fdeba8d9204ca8d2
Dismas <bandadismas@gmail.com>
2021-04-17 12:10:23 +0200

    feat: Tell a user to login in to dislike a comment

--- src/features/comments/DislikeButton.js
+++ src/features/comments/DislikeButton.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ThumbDownOutlinedIcon from '@material-ui/icons/ThumbDownOutlined';
 import ThumbDownIcon from '@material-ui/icons/ThumbDown';
+import Tooltip from '@material-ui/core/Tooltip';
 import Button from '@material-ui/core/Button';
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
 import { useSelector, useDispatch } from 'react-redux';
 import { unwrapResult } from '@reduxjs/toolkit';
 
 import {dislikeComment} from './commentsSlice';
 
 export const DislikeButton = ({comment}) => {
+    const [open, setOpen] = useState(false);
     const dispatch = useDispatch();
     const user = useSelector(state => state.currentUser);
 
@@ -15,6 +18,10 @@ export const DislikeButton = ({comment}) => {
         'Authorization': `Bearer ${user.token}`,
         'Content-Type': 'application/json'
         };
+    
+    let content;
+    
+    const includes = comment.dislikes.includes(user.currentUser._id);
 
     const handleClick = async () => {
         if (Object.keys(user.currentUser).length!==0) {
@@ -28,12 +35,14 @@ export const DislikeButton = ({comment}) => {
               } catch (err) {
                 console.error('Failed to dislike comment: ', err);
               } 
+        } else {
+            setOpen(true);
         }
     }
 
-    let content;
-
-    const includes = comment.dislikes.includes(user.currentUser._id);
+    const handleTooltipClose = () => {
+        setOpen(false);
+    };
 
     if (includes) {
         content = <ThumbDownIcon/>
@@ -42,7 +51,23 @@ export const DislikeButton = ({comment}) => {
     }
 
     return(
-        <Button onClick={handleClick}>{content}</Button>
+        <ClickAwayListener onClickAway={handleTooltipClose}>
+            <span className="mr-2">
+              <Tooltip
+                PopperProps={{
+                  disablePortal: true,
+                }}
+                onClose={handleTooltipClose}
+                open={open}
+                disableFocusListener
+                disableHoverListener
+                disableTouchListener
+                title="You must be signed in in order to dislike a comment"
+              >
+                <Button onClick={handleClick}>{content}</Button>
+              </Tooltip>
+            </span>
+          </ClickAwayListener>
     );
 }
 
